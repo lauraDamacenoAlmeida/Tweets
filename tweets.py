10b7a7a86de3de73ce2c7e9cb2298ff66fa7a589
Laura Damaceno de Almeida <41930302+lauraDamacenoAlmeida@users.noreply.github.com>
2019-05-09 10:29:36 -0300

    first commit
     with the connection API and capture data

---
+++ tweets.py
@@ -0,0 +1,18 @@
+from TwitterSearch import *
+
+try:
+    tweets = TwitterSearch(
+        consumer_key = 'XXXXXXXXX',
+        consumer_secret = 'XXXXX',
+        access_token = 'XXXXXXX',
+        access_token_secret = 'XXXXXXX'
+    )
+    pesquisa = TwitterSearchOrder()
+    pesquisa.set_keywords(['BTS'])
+    pesquisa.set_language('pt')
+
+    for tweet in tweets.search_tweets_iterable(pesquisa):
+        print('@{0} tweeted: {1}'.format(tweet['user']['screen_name'], tweet['text']))
+
+except TwitterSearchException as e:
+    print(e)
